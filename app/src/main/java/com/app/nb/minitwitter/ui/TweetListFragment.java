515d1913e50863a883c1ff4c42bed077b1aea078
namelessbliss <anonimo2911.2012@gmail.com>
2019-10-01 00:55:45 -0500

    Diseño del Adaptador, Usando Glide y CircleImageView

--- app/src/main/java/com/app/nb/minitwitter/ui/TweetListFragment.java
+++ app/src/main/java/com/app/nb/minitwitter/ui/TweetListFragment.java
@@ -2,35 +2,32 @@ package com.app.nb.minitwitter.ui;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.app.nb.minitwitter.R;
-import com.app.nb.minitwitter.ui.dummy.DummyContent;
-import com.app.nb.minitwitter.ui.dummy.DummyContent.DummyItem;
+import com.app.nb.minitwitter.retrofit.response.Tweet;
 
 import java.util.List;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
- * interface.
- */
 public class TweetListFragment extends Fragment {
 
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
     // TODO: Customize parameters
     private int mColumnCount = 1;
-    private OnListFragmentInteractionListener mListener;
+
+    private RecyclerView recyclerView;
+
+    private TweetRecyclerViewAdapter adapter;
+
+    private List<Tweet> tweetList;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -66,47 +63,20 @@ public class TweetListFragment extends Fragment {
         // Set the adapter
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
+            recyclerView = (RecyclerView) view;
             if (mColumnCount <= 1) {
                 recyclerView.setLayoutManager(new LinearLayoutManager(context));
             } else {
                 recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
             }
-            recyclerView.setAdapter(new TweetRecyclerViewAdapter(DummyContent.ITEMS, mListener));
+            loadTweetData();
+
         }
         return view;
     }
 
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnListFragmentInteractionListener) {
-            mListener = (OnListFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnListFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onListFragmentInteraction(DummyItem item);
+    private void loadTweetData() {
+        adapter = new TweetRecyclerViewAdapter(getActivity(), tweetList);
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/app/nb/minitwitter/ui/TweetRecyclerViewAdapter.java
+++ app/src/main/java/com/app/nb/minitwitter/ui/TweetRecyclerViewAdapter.java
@@ -1,30 +1,35 @@
 package com.app.nb.minitwitter.ui;
 
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.app.nb.minitwitter.ui.TweetListFragment.OnListFragmentInteractionListener;
-import com.app.nb.minitwitter.ui.dummy.DummyContent.DummyItem;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.app.nb.minitwitter.R;
+import com.app.nb.minitwitter.common.Constants;
+import com.app.nb.minitwitter.common.SharedPreferencesManager;
+import com.app.nb.minitwitter.retrofit.response.Like;
+import com.app.nb.minitwitter.retrofit.response.Tweet;
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
-/**
- * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
- * specified {@link OnListFragmentInteractionListener}.
- * TODO: Replace the implementation with code for your data type.
- */
 public class TweetRecyclerViewAdapter extends RecyclerView.Adapter<TweetRecyclerViewAdapter.ViewHolder> {
 
-    private final List<DummyItem> mValues;
-    private final OnListFragmentInteractionListener mListener;
+    private Context context;
+    private List<Tweet> mValues;
+    private String username;
 
-    public TweetRecyclerViewAdapter(List<DummyItem> items, OnListFragmentInteractionListener listener) {
+    public TweetRecyclerViewAdapter(Context context, List<Tweet> items) {
         mValues = items;
-        mListener = listener;
+        this.context = context;
+        //Obtiene el nombre usado almcenado en shared preferences
+        username = SharedPreferencesManager.getStringValue(Constants.PREF_USERNAME);
     }
 
     @Override
@@ -37,19 +42,32 @@ public class TweetRecyclerViewAdapter extends RecyclerView.Adapter<TweetRecycler
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
 
-        holder.mView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (null != mListener) {
-                    // Notify the active callbacks interface (the activity, if the
-                    // fragment is attached to one) that an item has been selected.
-                    mListener.onListFragmentInteraction(holder.mItem);
-                }
+        holder.tvUsername.setText(holder.mItem.getUser().getUsername());
+        holder.tvMessage.setText(holder.mItem.getMensaje());
+        holder.tvLikeCount.setText(holder.mItem.getLikes().size());
+
+        String photoUrl = holder.mItem.getUser().getPhotoUrl();
+        if (!photoUrl.equals("")) {
+            //Cargando imagen con Glide
+            Glide.with(context)
+                    .load("https://www.minitwitter.com/apiv1/uploads/photos/" + photoUrl)
+                    .into(holder.ivAvatar);
+        }
+
+
+        for (Like like : holder.mItem.getLikes()) {
+            if (like.getUsername().equals(username)) {
+                Glide.with(context)
+                        .load(R.drawable.ic_like_purple)
+                        .into(holder.ivLike);
+                // Cambia el color del contador de likes
+                holder.tvLikeCount.setTextColor(context.getResources().getColor(R.color.purple));
+                holder.tvLikeCount.setTypeface(null, Typeface.BOLD);
+                break;
             }
-        });
+        }
+
     }
 
     @Override
@@ -59,20 +77,21 @@ public class TweetRecyclerViewAdapter extends RecyclerView.Adapter<TweetRecycler
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
-        public final TextView mIdView;
-        public final TextView mContentView;
-        public DummyItem mItem;
+        public final ImageView ivAvatar;
+        public final ImageView ivLike;
+        public final TextView tvUsername;
+        public final TextView tvMessage;
+        public final TextView tvLikeCount;
+        public Tweet mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            mIdView = (TextView) view.findViewById(R.id.item_number);
-            mContentView = (TextView) view.findViewById(R.id.content);
-        }
-
-        @Override
-        public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
+            ivAvatar = view.findViewById(R.id.imageViewAvatar);
+            ivLike = view.findViewById(R.id.imageViewLike);
+            tvUsername = view.findViewById(R.id.textViewUsername);
+            tvMessage = view.findViewById(R.id.textViewMessage);
+            tvLikeCount = view.findViewById(R.id.textViewLike);
         }
     }
 }
