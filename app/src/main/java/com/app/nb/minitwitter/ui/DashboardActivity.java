a23f1a0a90b31b9d48a2ed7ba0baa2cb3dd909f9
namelessbliss <anonimo2911.2012@gmail.com>
2019-10-01 19:51:27 -0500

    Diseñando e implementando DialogFragmen para nuevo Tweet

--- app/src/main/java/com/app/nb/minitwitter/ui/DashboardActivity.java
+++ app/src/main/java/com/app/nb/minitwitter/ui/DashboardActivity.java
@@ -2,14 +2,18 @@ package com.app.nb.minitwitter.ui;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.nb.minitwitter.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -34,6 +38,16 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NuevoTweetDialogFragment dialogFragment = new NuevoTweetDialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "NuevoTweetDialogFragment");
+            }
+        });
 
         getSupportActionBar().hide();
 
---
+++ app/src/main/java/com/app/nb/minitwitter/ui/NuevoTweetDialogFragment.java
@@ -0,0 +1,33 @@
+package com.app.nb.minitwitter.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.app.nb.minitwitter.R;
+
+public class NuevoTweetDialogFragment extends DialogFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Aplica estilo para dialogo de pantalla completa
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialogStyle);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.nevo_tweet_dialog, container, false);
+
+        return view;
+    }
+}
