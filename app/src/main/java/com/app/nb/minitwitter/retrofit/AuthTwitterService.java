6196c7df2a24ccacee4333160a6a7c1eb78fab2e
namelessbliss <anonimo2911.2012@gmail.com>
2019-10-04 23:46:02 -0500

    Response funcion para subir foto de perfil

--- app/src/main/java/com/app/nb/minitwitter/retrofit/AuthTwitterService.java
+++ app/src/main/java/com/app/nb/minitwitter/retrofit/AuthTwitterService.java
@@ -2,18 +2,22 @@ package com.app.nb.minitwitter.retrofit;
 
 import com.app.nb.minitwitter.retrofit.request.RequestCreateTweet;
 import com.app.nb.minitwitter.retrofit.request.RequestUserProfile;
+import com.app.nb.minitwitter.retrofit.response.ResponseUploadPhoto;
 import com.app.nb.minitwitter.retrofit.response.ResponseUserProfile;
 import com.app.nb.minitwitter.retrofit.response.Tweet;
 import com.app.nb.minitwitter.retrofit.response.TweetDeleted;
 
 import java.util.List;
 
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface AuthTwitterService {
@@ -66,5 +70,9 @@ public interface AuthTwitterService {
      */
     @PUT("users/profile")
     Call<ResponseUserProfile> updateProfile(@Body RequestUserProfile requestUserProfile);
+
+    @Multipart
+    @POST("users/uploadprofilephoto")
+    Call<ResponseUploadPhoto> uploadProfilePhoto(@Part("file\"; filename=\"photo.jpg\" ") RequestBody file);
 
 }
